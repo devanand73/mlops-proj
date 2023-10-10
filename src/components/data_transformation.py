eafe35bfa00c4a36989c6c1c2e79d44e4e6a5880
devanand73 <devanand73@yahoo.co.uk>
2023-10-10 17:12:04 +0530

    working baseline v1

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,33 +1,39 @@
+# Import necessary libraries and modules
 import sys
 from dataclasses import dataclass
-
-import numpy as np 
+import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+"""The provided Python code defines a data transformation pipeline for machine learning.
+It includes the creation of numerical and categorical data transformers using scikit-learn's Pipeline and ColumnTransformer.
+The code reads training and testing data, applies these transformers to preprocess the data, 
+and then saves the preprocessing object to a Picklefile for later use."""
 
+# Import custom exceptions, logging, and other utilities
 from src.exception import CustomException
 from src.logger import logging
 import os
-
 from src.utils import save_object
 
+# Define a data class to hold configuration settings
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path = os.path.join('artifacts', 'proprocessor.pkl')
 
+# Create a DataTransformation class
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config=DataTransformationConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
     def get_data_transformer_object(self):
         '''
-        This function is responsible for data transformation
-        
+        This function is responsible for data transformation.
         '''
         try:
+            # Define numerical and categorical columns
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
                 "gender",
@@ -37,70 +43,71 @@ class DataTransformation:
                 "test_preparation_course",
             ]
 
-            num_pipeline= Pipeline(
+            # Create pipelines for numerical and categorical data
+            num_pipeline = Pipeline(
                 steps=[
-                ("imputer",SimpleImputer(strategy="median")),
-                ("scaler",StandardScaler())
-
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
                 ]
             )
 
-            cat_pipeline=Pipeline(
-
+            cat_pipeline = Pipeline(
                 steps=[
-                ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("one_hot_encoder",OneHotEncoder()),
-                ("scaler",StandardScaler(with_mean=False))
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler", StandardScaler(with_mean=False))
                 ]
-
             )
 
             logging.info(f"Categorical columns: {categorical_columns}")
             logging.info(f"Numerical columns: {numerical_columns}")
 
-            preprocessor=ColumnTransformer(
+            # Create a ColumnTransformer to apply different transformers to different columns
+            preprocessor = ColumnTransformer(
                 [
-                ("num_pipeline",num_pipeline,numerical_columns),
-                ("cat_pipelines",cat_pipeline,categorical_columns)
-
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("cat_pipeline", cat_pipeline, categorical_columns)
                 ]
-
-
             )
 
             return preprocessor
-        
-        except Exception as e:
-            raise CustomException(e,sys)
-        
-    def initiate_data_transformation(self,train_path,test_path):
 
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def initiate_data_transformation(self, train_path, test_path):
         try:
-            train_df=pd.read_csv(train_path)
-            test_df=pd.read_csv(test_path)
+            # Read train and test data from CSV files
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
 
             logging.info("Read train and test data completed")
 
             logging.info("Obtaining preprocessing object")
 
-            preprocessing_obj=self.get_data_transformer_object()
+            # Get the data transformation object
+            preprocessing_obj = self.get_data_transformer_object()
 
-            target_column_name="math_score"
+            # Define target column name and numerical columns
+            target_column_name = "math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df=train_df[target_column_name]
+            # Separate input features and target features for training and testing data
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df=test_df[target_column_name]
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            # Apply the data transformation to input features
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
+            # Combine input features and target features into arrays
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
             ]
@@ -108,11 +115,10 @@ class DataTransformation:
 
             logging.info(f"Saved preprocessing object.")
 
+            # Save the preprocessing object to a file
             save_object(
-
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
-
             )
 
             return (
@@ -121,4 +127,4 @@ class DataTransformation:
                 self.data_transformation_config.preprocessor_obj_file_path,
             )
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,7 +2,13 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
+"""The provided Python code consists of two classes. 
 
+1. `PredictPipeline` is a class that loads a machine learning model and a data preprocessor from specified file paths, applies the preprocessor to input features, and uses the model to make predictions. It handles exceptions using a custom exception class.
+
+2. `CustomData` is a class representing custom data with attributes like gender, race_ethnicity, parental_level_of_education, lunch, test_preparation_course, reading_score, and writing_score. It provides a method to convert this data into a Pandas DataFrame.
+
+This code is designed for making predictions using a trained machine learning model on custom input data."""
 
 class PredictPipeline:
     def __init__(self):
